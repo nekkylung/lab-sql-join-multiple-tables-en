e4fdb8134df7481f7c2b7d54421fcd084551d22b
nekkylung <nekkytang@gmail.com>
2025-08-05 17:03:14 +0200

    lab2

--- solutions.sql
+++ solutions.sql
@@ -1,7 +1,4 @@
-# Add you solution queries below:
-# Sakila Database Lab Solutions
-USE sakila;
-# 1. Write a query to display for each store its store ID, city, and country.
+-- 1. Write a query to display for each store its store ID, city, and country.
 SELECT 
     s.store_id,
     c.city,
@@ -11,7 +8,7 @@ JOIN address a ON s.address_id = a.address_id
 JOIN city c ON a.city_id = c.city_id
 JOIN country co ON c.country_id = co.country_id;
 
-# 2. Write a query to display how much business, in dollars, each store brought in.
+--2. Write a query to display how much business, in dollars, each store brought in.
 SELECT 
     s.store_id,
     SUM(p.amount) AS total_revenue
@@ -22,7 +19,7 @@ JOIN payment p ON r.rental_id = p.rental_id
 GROUP BY s.store_id
 ORDER BY s.store_id;
 
-# 3. What is the average running time of films by category?
+--3. What is the average running time of films by category?
 SELECT 
     c.name AS category,
     AVG(f.length) AS avg_running_time
@@ -32,7 +29,7 @@ JOIN film f ON fc.film_id = f.film_id
 GROUP BY c.category_id, c.name
 ORDER BY c.name;
 
-# 4. Which film categories are longest?
+--4. Which film categories are longest?
 SELECT 
     c.name AS category,
     AVG(f.length) AS avg_running_time
@@ -42,7 +39,7 @@ JOIN film f ON fc.film_id = f.film_id
 GROUP BY c.category_id, c.name
 ORDER BY avg_running_time DESC;
 
-# 5. Display the most frequently rented movies in descending order.
+--5. Display the most frequently rented movies in descending order.
 SELECT 
     f.title,
     COUNT(r.rental_id) AS rental_count
@@ -52,7 +49,7 @@ JOIN rental r ON i.inventory_id = r.inventory_id
 GROUP BY f.film_id, f.title
 ORDER BY rental_count DESC;
 
-# 6. List the top five genres in gross revenue in descending order.
+--6. List the top five genres in gross revenue in descending order.
 SELECT 
     c.name AS genre,
     SUM(p.amount) AS gross_revenue
@@ -66,16 +63,15 @@ GROUP BY c.category_id, c.name
 ORDER BY gross_revenue DESC
 LIMIT 5;
 
-# 7. Is "Academy Dinosaur" available for rent from Store 1?
+--7. Is "Academy Dinosaur" available for rent from Store 1
 SELECT 
     f.title,
     s.store_id,
     COUNT(i.inventory_id) AS total_copies,
-    SUM(CASE WHEN inventory_in_stock(i.inventory_id) THEN 1 ELSE 0 END) AS available_copies
+    SUM(CASE WHEN i.inventory_id THEN 1 ELSE 0 END) AS available_copies
 FROM film f
 JOIN inventory i ON f.film_id = i.film_id
 JOIN store s ON i.store_id = s.store_id
-WHERE f.title = 'Academy Dinosaur'
-AND s.store_id = 1
+WHERE f.title = 'ACADEMY DINOSAUR' AND s.store_id = 1
 GROUP BY f.film_id, f.title, s.store_id;
 
